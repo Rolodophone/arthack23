69a2e01faa7ccf5dd8b290252a679141e68d75b1
Rolodophone <rolodophone@gmail.com>
2023-02-18 20:35:10 +0000

    phases 2 & 3

--- src/main/kotlin/DevTools.kt
+++ src/main/kotlin/DevTools.kt
@@ -4,7 +4,7 @@ import org.openrndr.color.ColorRGBa
 import org.openrndr.extra.noise.gradient3D
 import org.openrndr.extra.noise.simplex4D
 
-private const val DRAW_DEBUG_INFO = false
+private const val DRAW_DEBUG_INFO = true
 
 class DevTools(private val program: Program,
 			   private val nebula: Nebula) {
@@ -34,50 +34,50 @@ class DevTools(private val program: Program,
 			//debug text background
 			fill = ColorRGBa.BLACK.opacify(0.5)
 			stroke = null
-			rectangle(0.0, 0.0, 210.0, 260.0)
+			rectangle(0.0, 0.0, 100.0, 40.0)
 
 			//debug text
 			fill = ColorRGBa.WHITE
 			text("frameNumber: ${nebula.frameNumber}", 20.0, 20.0)
-			text("friction: ${nebula.mainGroup.friction}", 20.0, 60.0)
-			text("simplexSeed: ${nebula.mainGroup.simplexSeed}", 20.0, 40.0)
-			text("simplexScale: ${nebula.mainGroup.simplexScale}", 20.0, 80.0)
-			text("simplexSpeed: ${nebula.mainGroup.simplexSpeed}", 20.0, 100.0)
-			text("simplexWeight: ${nebula.mainGroup.simplexWeight}", 20.0, 120.0)
-			text("nearScreenEdgeAccel: ${nebula.mainGroup.nearScreenEdgeAccel}", 20.0, 140.0)
-			text("gravityWeight: ${nebula.mainGroup.gravityWeight}", 20.0, 160.0)
-			text("repulsionWeight: ${nebula.mainGroup.repulsionWeight}", 20.0, 180.0)
-			text("contourAttraction: ${nebula.mainGroup.contourAttraction}", 20.0, 200.0)
-			text("contourAccel: ${nebula.mainGroup.contourAccel}", 20.0, 220.0)
-			text("totalVelWeight: ${nebula.mainGroup.totalVelWeight}", 20.0, 240.0)
-
-			//gradient indicator
-			val gradient = gradient3D(
-				noise = simplex4D,
-				seed = nebula.mainGroup.simplexSeed,
-				x = nebula.mainGroup.simplexScale * program.mouse.position.x,
-				y = nebula.mainGroup.simplexScale * program.mouse.position.y,
-				z = nebula.mainGroup.simplexSpeed * nebula.frameNumber.toDouble()
-			).xy
-			stroke = ColorRGBa.WHITE
-			lineSegment(program.mouse.position,
-						program.mouse.position + gradient * 10.0)
-			lineSegment(program.mouse.position + gradient * 10.0,
-						program.mouse.position + gradient * 10.0 + gradient.rotate(145.0) * 4.0)
-			lineSegment(program.mouse.position + gradient * 10.0,
-						program.mouse.position + gradient * 10.0 + gradient.rotate(215.0) * 4.0)
-
-			nebula.auxiliaryGroup.contour?.let { contour ->
-				//draw contour
-				stroke = ColorRGBa.WHITE
-                fill = null
-				contour(contour)
-
-				//draw projected point
-				stroke = null
-				fill = ColorRGBa.RED
-				circle(contour.nearestPatch(program.mouse.position).position, 1.0)
-			}
+//			text("friction: ${nebula.mainGroup.friction}", 20.0, 60.0)
+//			text("simplexSeed: ${nebula.mainGroup.simplexSeed}", 20.0, 40.0)
+//			text("simplexScale: ${nebula.mainGroup.simplexScale}", 20.0, 80.0)
+//			text("simplexSpeed: ${nebula.mainGroup.simplexSpeed}", 20.0, 100.0)
+//			text("simplexWeight: ${nebula.mainGroup.simplexWeight}", 20.0, 120.0)
+//			text("nearScreenEdgeAccel: ${nebula.mainGroup.nearScreenEdgeAccel}", 20.0, 140.0)
+//			text("gravityWeight: ${nebula.mainGroup.gravityWeight}", 20.0, 160.0)
+//			text("repulsionWeight: ${nebula.mainGroup.repulsionWeight}", 20.0, 180.0)
+//			text("contourAttraction: ${nebula.mainGroup.contourAttraction}", 20.0, 200.0)
+//			text("contourAccel: ${nebula.mainGroup.contourAccel}", 20.0, 220.0)
+//			text("totalVelWeight: ${nebula.mainGroup.totalVelWeight}", 20.0, 240.0)
+//
+//			//gradient indicator
+//			val gradient = gradient3D(
+//				noise = simplex4D,
+//				seed = nebula.mainGroup.simplexSeed,
+//				x = nebula.mainGroup.simplexScale * program.mouse.position.x,
+//				y = nebula.mainGroup.simplexScale * program.mouse.position.y,
+//				z = nebula.mainGroup.simplexSpeed * nebula.frameNumber.toDouble()
+//			).xy
+//			stroke = ColorRGBa.WHITE
+//			lineSegment(program.mouse.position,
+//						program.mouse.position + gradient * 10.0)
+//			lineSegment(program.mouse.position + gradient * 10.0,
+//						program.mouse.position + gradient * 10.0 + gradient.rotate(145.0) * 4.0)
+//			lineSegment(program.mouse.position + gradient * 10.0,
+//						program.mouse.position + gradient * 10.0 + gradient.rotate(215.0) * 4.0)
+//
+//			nebula.auxiliaryGroup.contour?.let { contour ->
+//				//draw contour
+//				stroke = ColorRGBa.WHITE
+//                fill = null
+//				contour(contour)
+//
+//				//draw projected point
+//				stroke = null
+//				fill = ColorRGBa.RED
+//				circle(contour.nearestPatch(program.mouse.position).position, 1.0)
+//			}
 		}
 	}
 }
--- src/main/kotlin/Nebula.kt
+++ src/main/kotlin/Nebula.kt
@@ -2,7 +2,11 @@ import org.openrndr.Program
 import org.openrndr.draw.DrawQuality
 import org.openrndr.draw.loadImage
 import org.openrndr.extra.noise.Random
+import org.openrndr.extra.noise.uniformRing
 import org.openrndr.math.Matrix55
+import org.openrndr.math.Vector2
+import org.openrndr.shape.Circle
+import org.openrndr.shape.LineSegment
 import org.openrndr.shape.contour
 
 class Nebula(private val program: Program) {
@@ -18,25 +22,60 @@ class Nebula(private val program: Program) {
             friction = 0.997,
             simplexWeight = 0.01,
             gravityWeight = 0.00002,
+            repulsionWeight = 0.00002,
         )
 	}
 
 	fun update() {
 		when (frameNumber) {
-            in 0..399 -> {
-                repeat(10) {
+            in 0 until 500 -> {
+                repeat(6) {
                     val variance = Random.double()
                     val newParticle = Particle(0.0, program.height/2.0 + 100*variance)
-                    newParticle.vel.set(3.0, 3*variance)
+                    newParticle.vel.set(2.0, 1.3*variance)
                     mainGroup.particles.add(newParticle)
                 }
-                repeat(10) {
+                repeat(6) {
                     val variance = Random.double()
                     val newParticle = Particle(program.width.toDouble(),
                         program.height/2.0 + 100*variance)
-                    newParticle.vel.set(-3.0, 3*variance)
+                    newParticle.vel.set(-2.0, 1.3*variance)
                     mainGroup.particles.add(newParticle)
                 }
+            }
+            1500 -> {
+                mainGroup.resetParameters(
+                    friction = 0.997,
+                    contourAttraction = 0.03,
+                    contourAttractionReach = 1000.0,
+                    contourAccel = 0.01,
+                    contour = Circle(program.width / 2.0, program.height / 2.0, 200.0).contour,
+                )
+            }
+            in 1500 until 5000 -> {
+                mainGroup.contourAccel += 0.00002
+            }
+            5000 -> {
+                mainGroup.friction = 0.99
+                auxiliaryGroup.resetParameters(
+                    contourAttraction = 0.002,
+                    contourAttractionReach = 1000.0,
+                    contour = LineSegment(program.width/2.0, program.height/2.0 - 110.0,
+                    program.width/2.0, program.height/2.0 + 110.0).contour
+                )
+            }
+            in 5000 until 5500 -> {
+                repeat(11) {
+                    mainGroup.particles.removeFirst()
+                }
+                repeat(4) {
+                    val pos = MutableVector(program.width / 2.0, program.height / 2.0) +
+                            Vector2.uniformRing(innerRadius = 95.0, outerRadius = 105.0)
+                    auxiliaryGroup.particles.add(Particle(pos))
+                }
+            }
+            in 5500 until 6500 -> {
+                auxiliaryGroup.contourAttraction += 0.00001
             }
 		}
 
